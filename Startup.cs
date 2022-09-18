249e77495c80d611f4e8d024418666ef33d7a037
SoftwizCircle <SoftwizCircle@gmail.com>
2022-09-18 19:36:56 +0530

    Adding Area

--- Startup.cs
+++ Startup.cs
@@ -51,6 +51,10 @@ namespace SoftWizECommerce
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "admin",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
